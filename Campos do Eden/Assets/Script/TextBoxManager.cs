b84050e7a1550c65c80163dec8321d72950a0cd7
gusjasponde <gustavocalonico@ufrj.br>
2016-10-02 00:07:22 -0300

    Text Box

---
+++ Campos do Eden/Assets/Script/TextBoxManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextBoxManager : MonoBehaviour
+{
+
+    public GameObject textBox;
+    //public Text theText = null;
+    public Text_Writer writer;
+
+    public int currentLine = 0;
+    public int endAtLine;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    // Use this for initialization
+    void Start()
+    {
+        textBox.SetActive(true);
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if(endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+
+        writer.Write(textLines[currentLine]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //theText.text = textLines[currentLine];
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(writer.trySkip())
+            {
+                currentLine += 1;
+                if (currentLine > endAtLine)
+                {
+                    textBox.SetActive(false);
+                    writer.Write(string.Empty);
+                    writer.StopAllCoroutines();
+                }
+                else
+                {
+                    writer.Write(textLines[currentLine]);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Campos do Eden/Assets/Script/Text_Writer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Text_Writer : MonoBehaviour {
+
+    private enum TextState { Writting, Finished }
+    private float currentSpeed;
+
+    public float speed;
+
+    public AudioClip typeSound1, typeSound2;
+    
+    private TextState currentState;
+
+    string message;
+    public Text textComp;
+           
+	public void Write (string text) {
+        StopCoroutine("TypeText");
+        message = text;
+        StartCoroutine("TypeText");
+    }
+
+    public bool trySkip()
+    {
+        switch (currentState)
+        {
+            case TextState.Writting:
+                currentState = TextState.Finished;
+                StopCoroutine("TypeText");
+                textComp.text = message;
+                break;
+
+            case TextState.Finished:
+                return true;
+        }
+        return false;
+    }
+
+    IEnumerator TypeText()
+    {
+        currentSpeed = speed;
+        textComp.text = string.Empty;
+        currentState = TextState.Writting;
+
+        foreach(char letter in message.ToCharArray()){
+            textComp.text += letter;
+            if (typeSound1 && typeSound2)
+                //SoundManager.instance.RandomizeSfx(typeSound1, typeSound2);
+                yield return 0;
+
+
+            yield return new WaitForSeconds(currentSpeed);
+        }
+        textComp.text = message;
+        currentState = TextState.Finished;
+    }
+}
